43d5a63d80051bcd73c5cdaa7e9b4e5c3b72d87f
Javi Alcantara <javier.alcantara@linkeit.com>
2019-07-12 00:08:54 +0200

    feat: Create articles list component

---
+++ components/ArticleList.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class='article-list'>
+    <div
+    class='element' v-for='(img, index) in images' :key='index'>
+      <img class='absolute' :src='getImage(img)' alt="">
+      <div class='absolute shadow'></div>
+        <div class='article__info'>
+          <span>10 minutos de lectura</span>
+          <NLink :to="{name: 'article-id', params: { id: img } }">
+            <h2>Crear chatbot con amazon lex</h2>
+          </NLink>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'article-list',
+
+  data: () => ({
+    images: ['chatbot','code','chatbot','code','chatbot','code', 'chatbot', 'code']
+  }),
+
+  methods: {
+    getImage(name) {
+      return require(`~/assets/${name}.png`)
+    }
+  }
+}
+</script>
+
+<style lang='sass' scoped>
+  @import '~assets/sass/media-queries'
+
+  .absolute
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    height: 100%
+    width: 100%
+  
+  .article-list
+    margin-top: 1em
+    display: flex
+    display: flex
+    flex-wrap: wrap
+
+    .element
+      height: 13em
+      flex: 0 0 100%
+      background: #ececec
+      position: relative
+      color: #fff
+
+      @include for-tablet-portrait-up
+        flex: 0 0 50%
+      @include for-tablet-landscape-up
+        flex: 0 0 33%
+
+      &:hover
+        .shadow
+          background: linear-gradient(to bottom, rgba(185, 185, 185, 0) 7%, rgba(0, 0, 0, 0.8) 57%)
+        .article__info
+          bottom: .5em
+        
+
+      img
+        object-fit: cover
+
+      .shadow
+        background: linear-gradient(to bottom, rgba(185, 185, 185, 0) 7%, rgba(0, 0, 0, 0.6) 86%)
+
+      .article__info
+        transition: all .1s linear
+        position: absolute
+        left: 0
+        bottom: 0
+        padding: .5em
+
+        a 
+          text-decoration:  none
+          &:visited
+            text-decoration:  none
+
+        h2
+          text-shadow: 1px 1px #383838
+          color: #fff
+
+        span
+          font-size: .9em
+          color: #dadada
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,21 @@
 <template>
-  <section class="container">
+  <page>
     <div>
-      <img src='@/static/javiscript-logo.svg' />
-      <h2 class="subtitle">
-        Blog de programación
-      </h2>
-      <div class="links">
-        <a
-          href="https://github.com/javialcantaras"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+      <article-list/>
     </div>
-  </section>
+  </page>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Page from '~/components/Page'
+import ArticleList from '~/components/ArticleList'
 
 export default {
-  components: {
-    AppLogo
-  }
+  components: { Page, ArticleList }
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang='sass'>
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
 
